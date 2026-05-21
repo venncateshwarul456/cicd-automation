18269f850990f7539c6cefa8e29afeff1818cdb2
venncateshwarul456 <venncateshwarul33@gmail.com>
2026-05-21 16:28:52 +0530

    modified source code

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'vivaan Technologies pvt ltd!';
+    return 'Vivaan Technologies pvt ltd!';
   }
 }
